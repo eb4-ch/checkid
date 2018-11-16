497aae06208d7de4321da4dff8e21017e156943c
caa <caa@ti8m.ch>
2018-11-16 10:53:57 +0100

    Add aws functions

--- todoListApi/api/controllers/check-id-picture.controller.js
+++ todoListApi/api/controllers/check-id-picture.controller.js
@@ -1,7 +1,9 @@
 'use strict';
+var awsController = require('./awscontroller.js')
 module.exports = function(app, options) {
     app.post('/upload', options.array("uploads[]", 12), function(req, res) {
-        console.log('files', req.files);
-        res.send(req.files);
+        //console.log('files', req.files);
+        awsController.f(req,res);
+        // res.send(req.files);
     });
 }
